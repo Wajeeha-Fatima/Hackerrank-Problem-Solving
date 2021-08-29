5a5601e81875e78e24e27c636331ccec21b79f59
Wajeeha-Fatima <77614915+Wajeeha-Fatima@users.noreply.github.com>
2021-08-30 00:53:05 +0500

    Create Basic Data Types.cpp

---
+++ Basic Data Types.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int i;
+    long int l;
+    float f;
+    double d;
+    char c;
+    cin >> i >> l >> c >> f >> d;
+    cout << i << endl;
+    cout << l << endl;
+    cout << c << endl;
+    cout << setprecision(8) << f << endl;
+    cout << setprecision(16) << d << endl;
+
+    return 0;
+}
